9e60330b7b3fee0db88c18ee30f3176102a131f5
Luc M. Davis <lmdavis@caltech.edu>
2019-05-31 11:18:35 -0700

    Update hazard.h

--- include/hazard.h
+++ include/hazard.h
@@ -20,15 +20,15 @@ void spike_hazard_init(Vector position, Scene* scene);
 //partial destroy_body collision type if out of Lives, otherwise, repel_body
 //Empty force exerted (does not exert a force)
 
-void gravity_well_init(Vector position, double mass, Body* player, Scene* scene);
+void gravity_hazard_init(Vector position, double mass, Body* player, Scene* scene);
 //Empty collision type (only collides with spikes, to be destroyed)
 //Exerts gravity on the player
 
-void moving_ball_init(Vector position, Vector velocity, double mass, Scene* scene);
+void moving_ball_hazard_init(Vector position, Vector velocity, double mass, Scene* scene);
 //physics_collision collision type (spikes still destroy this and are unmoved)
 //Empty force exerted (does not exert a force)
 
-void spring_platforms_init(Vector position1, Vector position2, Vector dimension, double k, Scene* scene);
+void spring_platforms_hazard_init(Vector position1, Vector position2, Vector dimension, double k, Scene* scene);
 //repel_body collision type, if both platforms touching the player, partial destroy_body
 //Spring force exerted on the other platform (generated in a pair)
 
