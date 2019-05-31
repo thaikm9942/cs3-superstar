35334d95d667b985e95cea8843cba418ad183709
Luc M. Davis <lmdavis@caltech.edu>
2019-05-30 19:15:31 -0700

    Update hazard.h

--- include/hazard.h
+++ include/hazard.h
@@ -14,19 +14,19 @@ typedef struct hazard {
     ForceCreator force_type;
 } Hazard;
 
-Hazard* spike_init(Vector position);
+Body* spike_init(Vector position);
 //partial destroy_body collision type if out of Lives, otherwise, repel_body
 //Empty force exerted (does not exert a force)
 
-Hazard* gravity_well_init(Vector position, int mass, int radius);
+Body* gravity_well_init(Vector position, double mass, Body* player);
 //Empty collision type (only collides with spikes, to be destroyed)
 //Exerts gravity on the player
 
-Hazard* moving_ball_init(Vector position, Vector velocity, int mass, int radius);
+Body* moving_ball_init(Vector position, Vector velocity, double mass);
 //physics_collision collision type (spikes still destroy this and are unmoved)
 //Empty force exerted (does not exert a force)
 
-Hazard* spring_platforms_init(Vector position, Vector dimension, int k);
+Body* spring_platforms_init(Vector position, Vector dimension, double k);
 //repel_body collision type, if both platforms touching the player, partial destroy_body
 //Spring force exerted on the other platform (generated in a pair)
 
