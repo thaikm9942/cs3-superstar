44d48ba51af2d9cb32f07f5656cdd86ca879f370
Luc M. Davis <lmdavis@caltech.edu>
2019-05-31 11:18:11 -0700

    Update hazard.c

--- library/hazard.c
+++ library/hazard.c
@@ -19,24 +19,24 @@ void spike_hazard_init(Vector position, Scene* scene){
     scene_add_body(spike_body, scene);
 }
 
-void gravity_well_init(Vector position, double mass, Body* player, Scene* scene){
+void gravity_hazard_init(Vector position, double mass, Body* player, Scene* scene){
     Body* grav_body = malloc(sizeof(Body);
-    grav_body = hazard_init(position, HAZARD_RADIUS, HAZARD_MASS, GRAV_COLOR, 1);
+    grav_body = gravity_ball_init(position, HAZARD_RADIUS, HAZARD_MASS, GRAV_COLOR, 1);
     scene_add_body(grav_body, scene);
     create_newtonian_gravity(scene, G, grav_body, player);
 }
 
-void moving_ball_init(Vector position, Vector velocity, double mass, Scene* scene){
+void moving_ball_hazard_init(Vector position, Vector velocity, double mass, Scene* scene){
     Body* moving_ball_body = malloc(sizeof(Body));
-    moving_ball_body = moving_ball_hazard_init(position, HAZARD_RADIUS, mass, BALL_COLOR, 1);
+    moving_ball_body = moving_ball_init(position, HAZARD_RADIUS, mass, BALL_COLOR, 1);
     scene_add_body(moving_ball_body, scene);
 }
 
-void spring_platforms_init(Vector position1, Vector position2, Vector dimension, double k, Scene* scene){
+void spring_platforms_hazard_init(Vector position1, Vector position2, Vector dimension, double k, Scene* scene){
     Body* spring_platform_body1 = malloc(sizeof(Body));
     Body* spring_platform_body2 = malloc(sizeof(Body));
-    spring_platform_body1 = platform_hazard_init(position1, dimension);
-    spring_platform_body2 = platform_hazard_init(position2, dimension);
+    spring_platform_body1 = platform_spring_init(position1, dimension);
+    spring_platform_body2 = platform_spring_init(position2, dimension);
     scene_add_body(spring_platform_body1, scene);
     scene_add_body(spring_platform_body2, scene);
     create_spring_force(scene, k, spring_platform_body1, spring_platform_body2);
