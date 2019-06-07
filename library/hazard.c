886564509fd10b5bd100ebf91efdace50dbb4c22
Luc M. Davis <lmdavis@caltech.edu>
2019-06-06 19:23:23 -0700

    Update hazard.c

--- library/hazard.c
+++ library/hazard.c
@@ -12,6 +12,7 @@ const double HAZARD_MASS = 100;
 // GRAV_COLOR and SPIKE_COLOR are both BLACK
 const RGBColor GRAV_COLOR = (RGBColor){0.0, 0.0, 0.0};
 const RGBColor SPIKE_COLOR = (RGBColor){0.0, 0.0, 0.0};
+const RGBColor BAD_BALL_COLOR = (RGBColor){0.0, 0.0, 1.0};
 #define G 6.67E-11 // N m^2 / kg^2
 #define G2 6.67E-3 // N m^2 / kg^2
 
@@ -41,7 +42,7 @@ void gravity_hazard_init(Vector position, Scene* scene){
       BodyType type = body_info_get_type(info);
       if(type == PLAYER){
         create_newtonian_gravity(scene, G, grav_body, body);
-        create_player_gravity_collision(scene, 1, body, grav_body);
+        //create_player_gravity_collision(scene, 1, body, grav_body);
       }
       if(type == SPIKE){
         create_partial_destructive_collision_with_life(scene, body, grav_body);
@@ -49,12 +50,25 @@ void gravity_hazard_init(Vector position, Scene* scene){
     }
 }
 
-/*
+
 void moving_ball_hazard_init(Vector position, Vector velocity, double mass, Scene* scene){
-    Body* moving_ball_body = moving_ball_init(position, HAZARD_RADIUS, mass, BALL_COLOR, 1);
-    scene_add_body(moving_ball_body, scene);
+    Body* moving_ball_body = moving_ball_init(position, HAZARD_RADIUS, mass, BAD_BALL_COLOR, 1);
+    body_set_velocity(moving_ball_body, vec_add(DEFAULT_HAZARD_VEL, velocity));
+    scene_add_body(scene, moving_ball_body);
+    for(size_t i = 0; i < scene_bodies(scene); i++){
+      Body* body = scene_get_body(scene, i);
+      BodyInfo* info = body_get_info(body);
+      BodyType type = body_info_get_type(info);
+      if(type == PLAYER){
+        //Moving Ball and Player collision type
+        create_physics_collision(scene, 0.5, moving_ball_body, body);
+      }
+      if(type == SPIKE){
+        create_partial_destructive_collision_with_life(scene, body, moving_ball_body);
+      }
+    }
 }
-*/
+
 
 /*
 void spring_platforms_hazard_init(Vector position1, Vector position2, Vector dimension, double k, Scene* scene){
